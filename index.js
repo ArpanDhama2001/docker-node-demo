6865b369b364a42947a5bdbc516dc2b58fd70bc5
Arpan Dhama <arpandhama2001@gmail.com>
2025-05-10 11:06:18 +0530

    Added env variables

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     return res.status(200).json({
